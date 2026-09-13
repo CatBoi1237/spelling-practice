e62669e7f2f63b44a617aadd3c1e467a13b5413e
CatBoi1237 <164852525+CatBoi1237@users.noreply.github.com>
2026-09-13 21:10:57 +1000

    Add QR joining to classroom lobby

--- frontend/src/pages/ClassroomShell.jsx
+++ frontend/src/pages/ClassroomShell.jsx
@@ -1,10 +1,12 @@
 import Classroom from "@/pages/Classroom";
 import ClassroomTeacherTools from "@/components/ClassroomTeacherTools";
+import ClassroomHostQr from "@/components/ClassroomHostQr";
 
 export default function ClassroomShell() {
   return (
     <>
       <Classroom />
+      <ClassroomHostQr />
       <ClassroomTeacherTools />
     </>
   );

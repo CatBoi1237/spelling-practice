d593d41a5f64689b90920b151d2fcc76ca83b37d
CatBoi1237 <164852525+CatBoi1237@users.noreply.github.com>
2026-09-13 21:08:11 +1000

    Wrap classroom with teacher controls

---
+++ frontend/src/pages/ClassroomShell.jsx
@@ -0,0 +1,11 @@
+import Classroom from "@/pages/Classroom";
+import ClassroomTeacherTools from "@/components/ClassroomTeacherTools";
+
+export default function ClassroomShell() {
+  return (
+    <>
+      <Classroom />
+      <ClassroomTeacherTools />
+    </>
+  );
+}

581ba418485a3f516a4e83ef88a538a252a67f01
CatBoi1237 <164852525+CatBoi1237@users.noreply.github.com>
2026-09-13 21:15:12 +1000

    Show Classroom results report after games

--- frontend/src/pages/ClassroomShell.jsx
+++ frontend/src/pages/ClassroomShell.jsx
@@ -1,11 +1,18 @@
+import { useParams } from "react-router-dom";
+
 import Classroom from "@/pages/Classroom";
 import ClassroomTeacherTools from "@/components/ClassroomTeacherTools";
 import ClassroomHostQr from "@/components/ClassroomHostQr";
+import ClassroomResultsReport from "@/components/ClassroomResultsReport";
 
 export default function ClassroomShell() {
+  const { code } = useParams();
+  const roomCode = (code || "").toUpperCase();
+
   return (
     <>
       <Classroom />
+      <ClassroomResultsReport roomCode={roomCode} />
       <ClassroomHostQr />
       <ClassroomTeacherTools />
     </>
